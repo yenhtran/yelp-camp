38de5710f2e0215aed6e12036aeec99733984344
Yen Tran <tran.yentran@gmail.com>
2016-12-02 19:47:40 +0000

    adds new campground feature

--- v1/app.js
+++ v1/app.js
@@ -1,5 +1,13 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    bodyParser = require('body-parser'),
+    campgrounds = [
+        { name: 'Samon Creek', image: "https://source.unsplash.com/sK1hW5knKkw/400X300"},
+        { name: 'Granite Chief', image: "https://source.unsplash.com/63Znf38gnXk/400X300"},
+        { name: 'Mountain Goat Rest', image: "https://source.unsplash.com/yOujaSETXlo/400X300"}
+    ];
+    
+app.use(bodyParser.urlencoded({extended: true}));
     
 app.set('view engine', 'ejs');
 
@@ -8,12 +16,20 @@ app.get('/', function(req, res){
 });
 
 app.get('/campgrounds', function(req, res){
-    var campgrounds = [
-        { name: 'Samon Creek', image: "https://source.unsplash.com/sK1hW5knKkw/400X300"},
-        { name: 'Granite Chief', image: "https://source.unsplash.com/63Znf38gnXk/400X300"},
-        { name: 'Mountain Goat Rest', image: "https://source.unsplash.com/yOujaSETXlo/400X300"}];
-        
     res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+app.post('/campgrounds', function(req, res){
+    var campName = req.body.name,
+        campImage = req.body.image,
+        newCampground = { name: campName, image: campImage };
+    
+    campgrounds.push(newCampground);
+    res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', function(req, res) {
+   res.render('new'); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
