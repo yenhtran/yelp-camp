8ee8df126d9a94d42933c20048002adebd138474
Yen Tran <tran.yentran@gmail.com>
2016-12-16 23:24:08 +0000

    Adds logout functionality

--- v1/app.js
+++ v1/app.js
@@ -152,6 +152,12 @@ app.post('/login', passport.authenticate('local', {
     
 });
 
+//logout route
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/campgrounds');
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('The YelpCamp Server has started!');
 });
