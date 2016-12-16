d3b7c915de802163a789bc26dd80f07e46e04bb1
Yen Tran <tran.yentran@gmail.com>
2016-12-16 22:20:29 +0000

    adds passport-local-mongoose plugin to user model

--- v1/models/user.js
+++ v1/models/user.js
@@ -1,7 +1,10 @@
 var mongoose = require("mongoose"),
+    passportLocalMongoose = require("passport-local-mongoose"),
     UserSchema = new mongoose.Schema({
         username: String,
         password: String
     });
+
+UserSchema.plugin(passportLocalMongoose);
     
 module.exports = mongoose.model('User', UserSchema);
