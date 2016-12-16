a497a719978b0b7788db275a536b88c392fc28fa
Yen Tran <tran.yentran@gmail.com>
2016-12-16 23:03:41 +0000

    Adds login functionality

--- v1/app.js
+++ v1/app.js
@@ -140,6 +140,18 @@ app.post('/register', function(req, res) {
     });
 });
 
+//show login form
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+//handle sign up logic
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/campgrounds',
+    failureRedirect: '/login'
+}), function(req, res) {
+    
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('The YelpCamp Server has started!');
 });
