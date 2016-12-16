325e838c58db5d80a20c11ca1cc9d578f4ae9c26
Yen Tran <tran.yentran@gmail.com>
2016-12-16 23:32:58 +0000

    Adds middleware to check and see if user isLoggedIn before submitting comment

--- v1/app.js
+++ v1/app.js
@@ -84,7 +84,7 @@ app.get('/campgrounds/:id', function(req, res) {
 // COMMENTS ROUTES
 // ===================
 
-app.get('/campgrounds/:id/comments/new', function(req, res){
+app.get('/campgrounds/:id/comments/new', isLoggedIn, function(req, res){
     Campground.findById(req.params.id, function(err, campground){
         if (err) {
            console.log(err);
@@ -94,7 +94,7 @@ app.get('/campgrounds/:id/comments/new', function(req, res){
     })
 });
 
-app.post('/campgrounds/:id/comments', function(req, res){
+app.post('/campgrounds/:id/comments', isLoggedIn, function(req, res){
     //lookup campground using id
     Campground.findById(req.params.id, function(err, campground) {
        if(err){
@@ -157,6 +157,13 @@ app.get('/logout', function(req, res){
     req.logout();
     res.redirect('/campgrounds');
 });
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+};
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('The YelpCamp Server has started!');
