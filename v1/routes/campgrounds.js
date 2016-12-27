5c64831f23334762c6bd49ab1c103264974c5934
Yen Tran <tran.yentran@gmail.com>
2016-12-27 21:21:56 +0000

    Owners of campgrounds can update/delete campgrounds when logged in

--- v1/routes/campgrounds.js
+++ v1/routes/campgrounds.js
@@ -30,7 +30,6 @@ router.post('/', isLoggedIn, function(req, res){
         if(err){
             console.log(err);
         } else {
-            console.log(newlyCreatedCampground);
             res.redirect('/campgrounds');
         }
     });
@@ -56,18 +55,14 @@ router.get('/:id', function(req, res) {
 });
 
 //EDIT CAMPGROUND ROUTE
-router.get('/:id/edit', function(req, res) {
+router.get('/:id/edit', checkCampgroundOwnership, function(req, res){
     Campground.findById(req.params.id, function(err, foundCampground){
-        if(err){
-            res.redirect('/campgrounds');
-        } else {
-            res.render('campgrounds/edit', {campground: foundCampground});
-        }
+        res.render('campgrounds/edit', {campground: foundCampground});
     });
 });
 
 //UPDATE CAMPGROUND ROUTE
-router.put('/:id', function(req, res){
+router.put('/:id', checkCampgroundOwnership, function(req, res){
     //find and update the correct campground
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
         if(err){
@@ -80,7 +75,7 @@ router.put('/:id', function(req, res){
 });
 
 //DESTROY CAMPGROUND ROUTE
-router.delete('/:id', function(req, res) {
+router.delete('/:id',checkCampgroundOwnership, function(req, res) {
     Campground.findByIdAndRemove(req.params.id, function(err){
         if(err){
             res.redirect('/campgrounds');
@@ -98,4 +93,23 @@ function isLoggedIn(req, res, next){
     res.redirect('/login');
 };
 
+function checkCampgroundOwnership(req, res, next) {
+    if(req.isAuthenticated()) {
+        //does user own the campground?
+        Campground.findById(req.params.id, function(err, foundCampground){
+            if(err){
+                res.redirect('back');
+            } else {
+                 //does user own the campground?
+                 if(foundCampground.author.id.equals(req.user._id)){
+                     next();
+                 } else {
+                     res.redirect('back');
+                 }
+            }
+        });
+    } else {
+        res.redirect('back');
+    }
+}
 module.exports = router;
