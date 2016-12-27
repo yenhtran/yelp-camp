c3d38d30d1aeb99b209b69a5fa5f60c95b374cfc
Yen Tran <tran.yentran@gmail.com>
2016-12-27 17:52:39 +0000

    Users can delete campgrounds

--- v1/routes/campgrounds.js
+++ v1/routes/campgrounds.js
@@ -79,6 +79,17 @@ router.put('/:id', function(req, res){
     //redirect somewhere(show page)
 });
 
+//DESTROY CAMPGROUND ROUTE
+router.delete('/:id', function(req, res) {
+    Campground.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect('/campgrounds');
+        } else {
+            res.redirect('/campgrounds');
+        }
+    })
+});
+
 //MIDDLEWARE
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
