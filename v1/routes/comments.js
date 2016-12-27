4df00516b050629087a784fde9c463a257e91963
Yen Tran <tran.yentran@gmail.com>
2016-12-27 23:21:52 +0000

    Users can edit comments for campgrounds

--- v1/routes/comments.js
+++ v1/routes/comments.js
@@ -39,6 +39,29 @@ router.post('/', isLoggedIn, function(req, res){
     });
 });
 
+//EDIT COMMENTS ROUTE
+router.get('/:comment_id/edit', function(req, res){
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+        if(err){
+            res.redirect('back');
+        } else {
+             res.render('comments/edit', {campground_id: req.params.id, comment: foundComment});
+        }
+    });
+   
+});
+
+//COMMENTS UPDATE
+router.put('/:comment_id', function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+        if (err) {
+            res.redirect('back');
+        } else {
+            res.redirect('/campgrounds/' + req.params.id);
+        }
+    });
+});
+
 //MIDDLEWARE
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
