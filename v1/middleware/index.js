067c572c9f3f442de55804079eb05da5e0202384
Yen Tran <tran.yentran@gmail.com>
2016-12-28 20:05:34 +0000

    Adds Flash error messages

--- v1/middleware/index.js
+++ v1/middleware/index.js
@@ -10,17 +10,20 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next) {
         //does user own the campground?
         Campground.findById(req.params.id, function(err, foundCampground){
             if(err){
+                req.flash('error', 'Campground not found');
                 res.redirect('back');
             } else {
                  //does user own the campground?
                  if(foundCampground.author.id.equals(req.user._id)){
                      next();
                  } else {
-                     res.redirect('back');
+                    req.flash('error', "You don't have permission to do that!");
+                    res.redirect('back');
                  }
             }
         });
     } else {
+        req.flash('error', 'You need to be logged in to do that!');
         res.redirect('back');
     }
 };
@@ -34,11 +37,13 @@ middlewareObj.checkCommentOwnership = function(req, res, next) {
                  if(foundComment.author.id.equals(req.user._id)){
                      next();
                  } else {
-                     res.redirect('back');
+                    req.flash('error', "You don't have permission to do that!");
+                    res.redirect('back');
                  }
             }
         });
     } else {
+        req.flash('error', 'You need to be logged in to do that!');
         res.redirect('back');
     }
 }
@@ -48,6 +53,7 @@ middlewareObj.isLoggedIn = function (req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
+    req.flash('error', 'You need to be logged in to do that!');
     res.redirect('/login');
 };
 
--- v1/routes/index.js
+++ v1/routes/index.js
@@ -18,10 +18,11 @@ router.post('/register', function(req, res) {
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
         if (err) {
-            console.log(err);
-            return res.render('register');
+            // req.flash('error', err.message);
+            return res.render('register', {'error': err.message});
         }
         passport.authenticate('local')(req, res, function(){
+            req.flash('success', 'Welcome to YelpCamp ' + user.username);
             res.redirect('/campgrounds');
         });
     });
@@ -42,15 +43,8 @@ router.post('/login', passport.authenticate('local', {
 //logout route
 router.get('/logout', function(req, res){
     req.logout();
+    req.flash('success', 'Logged you out!');
     res.redirect('/campgrounds');
 });
-
-//MIDDLEWARE
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect('/login');
-};
 
 module.exports = router;
